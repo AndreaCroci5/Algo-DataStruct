c0e31587f782331a8a4df9c5a5db270cd0a46270
Andrea <andrea5.croci@mail.polimi.it>
2025-02-15 22:54:19 +0100

    Add list struct and search-related functions to linked_list.c

--- data_structures/linked_list.c
+++ data_structures/linked_list.c
@@ -1,4 +1,43 @@
 //
 // Created by Andrea Croci on 15/02/25.
 //
+#include <stdlib.h>
 
+typedef struct Nd {
+    int data;
+    struct Nd * next;
+} Node;
+
+typedef Node * Nodeptr;
+
+void search_node_from_data(Nodeptr head, int data) {
+    Nodeptr ptr = head;
+    while (ptr != NULL && ptr->data != data) {
+        ptr = ptr->next;
+    }
+}
+
+/* For variant
+void search_node_from_data(Nodeptr head, int data) {
+    Nodeptr ptr;
+   for (ptr= head; ptr!=NULL && ptr->data!=data; ptr=ptr->next) {
+    }
+}
+*/
+
+int list_length(Nodeptr head) {
+    int counter = 0;
+    Nodeptr ptr;
+    for (ptr= head; ptr!=NULL; ptr=ptr->next) {
+        counter++;
+    }
+    return counter;
+}
+
+Nodeptr get_last_Node(Nodeptr head) {
+    Nodeptr ptr = head;
+    while (ptr != NULL) {
+        ptr = ptr->next;
+    }
+    return ptr;
+}

3de1c83aa880c9d3cd41812aea17030d2c8b0ff6
Andrea <andrea5.croci@mail.polimi.it>
2025-03-04 17:14:17 +0100

    QUEUE: Create queue.c file for the queue data structure implementation

---
+++ data_structures/queue.c
@@ -0,0 +1,11 @@
+/**
+* @file         queue.c
+ * @author      Andrea Croci
+ * @date        2025-03-04
+ * @brief       Implementation of a queue
+ *
+ * @details     This file implements a queue with
+ *              various operations such as enqueue, dequeue and destroy
+ *
+ */
+
